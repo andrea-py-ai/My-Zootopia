287051aba4e30bfef1071dc63a0df7ed28769d59
Andrea R. <218094644+andrea-py-ai@users.noreply.github.com>
2025-07-09 14:54:04 +0200

    Added basic code to animals_web_generator to read and print the jason file. Moved both the json and the html into folders for clearity.

---
+++ animals_web_generator.py
@@ -0,0 +1,11 @@
+import json
+
+
+def load_data(file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r", encoding="utf-8") as handle:
+        return json.load(handle)
+
+
+animals_data = load_data("data/animals_data.json")
+print(animals_data)
