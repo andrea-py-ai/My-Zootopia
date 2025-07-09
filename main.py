287efcf60f937d729dae54db8ee630df0d54c15f
Andrea R. <218094644+andrea-py-ai@users.noreply.github.com>
2025-07-09 15:48:28 +0200

    Changed main file to main.py and added main function and dunder main

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+
+
+def load_data(file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r", encoding="utf-8") as handle:
+        return json.load(handle)
+
+
+def main():
+    animals_data = load_data("data/animals_data.json")
+    print(animals_data)
+
+
+if __name__ == "__main__":
+    main()
