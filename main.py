f209fd49952dbe71993e8c9f915761da727c2e7e
Andrea R. <218094644+andrea-py-ai@users.noreply.github.com>
2025-07-09 16:42:32 +0200

    added basic functions layout and code for getting ainmal name info

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 from storage.storage_json import load_data
 
 
+def get_animal_name(animals):
+    animal_names = []
+
+    for animal in animals:
+        if animal:
+            animal_names.append(animal["name"])
+
+    return animal_names
+
+
+def get_animal_diet(animals):
+    pass
+
+def get_animal_first_location(animals):
+    pass
+
+
+def get_animal_type(animals):
+    pass
+
+
+def get_animal_info(animals):
+    all_animal_info = {"animal_names": get_animal_name,
+                       "animal_diet": get_animal_diet,
+                       "animal_first_location": get_animal_first_location,
+                       "animal_type": get_animal_type}
+
+    return all_animal_info
+
+
+def print_animal_info(animals):
+    pass
+
+
 def main():
     animals_data = load_data("data/animals_data.json")
-    print(animals_data)
+    animal_info = get_animal_info(animals_data)
+    print_animal_info(animal_info)
 
 
 if __name__ == "__main__":
