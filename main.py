dab2ff9c7956f352b818a8652841b2a8aed94c4f
Andrea R. <218094644+andrea-py-ai@users.noreply.github.com>
2025-07-09 16:03:08 +0200

    Added a package storage. Changed main.py to import function from storage.

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import json
-
-
-def load_data(file_path):
-    """ Loads a JSON file """
-    with open(file_path, "r", encoding="utf-8") as handle:
-        return json.load(handle)
+from storage.storage_json import load_data
 
 
 def main():
---
+++ storage/storage_json.py
@@ -0,0 +1,7 @@
+import json
+
+
+def load_data(file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r", encoding="utf-8") as handle:
+        return json.load(handle)
